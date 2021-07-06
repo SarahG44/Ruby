57b3ee2548e728abc494c61cbb8e3f55073bc73d
SarahG44 <s.godefroy@hotmail.fr>
2021-07-06 17:18:57 +0200

    ajout des fichiers

---
+++ exo_03.rb
@@ -0,0 +1,4 @@
+puts "Hey, tu es né en quelle année?"
+date_birth = gets.chomp
+puts "En #{date_birth}"
+puts "Donc en 2017, tu avais #{2017-1992} ans"
---
+++ exo_04.rb
@@ -0,0 +1,4 @@
+puts "Hey, tu es né en quelle année?"
+date_birth = gets.chomp
+puts "En #{date_birth}"
+puts "Donc tu auras 100 ans en #{1992+100}"
